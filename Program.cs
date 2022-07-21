4e5f2247e8f9e5c65986fac653e4da22480bae78
Ovidiu Rădoi <oradoi@cegid.com>
2022-07-21 09:45:11 +0300

    Added the third message

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!!!");
             Console.WriteLine("Hello again!");
+            Console.WriteLine("Hello for the third time!");
         }
     }
 }
